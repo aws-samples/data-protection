6a4629855b181a4e5fbfb690b9b22203ff7e1b2e
EC2 Default User <ec2-user@ip-10-0-0-192.us-west-2.compute.internal>
2019-12-05 00:15:41 +0000

    print error if subordinate certificate is not found

--- usecase-7/code/create-certs.py
+++ usecase-7/code/create-certs.py
@@ -15,37 +15,41 @@ def main():
         response = client.list_certificate_authorities(
             MaxResults=20
         )
+        SubArn = None
         max = len(response['CertificateAuthorities'])
         for x in range(0, max):
             if response['CertificateAuthorities'][x]['Status'] == 'ACTIVE' and response['CertificateAuthorities'][x]['Type'] == 'SUBORDINATE':
                 SubArn = str(response['CertificateAuthorities'][x]['Arn'])
         
-        #Get Subordinate CA CSR
-        csr = client.get_certificate_authority_csr(
-            CertificateAuthorityArn=SubArn
-        )
-            
-        #Generate private end entity certificates
-        for x in range(0, numCerts):
-            response = acmClient.request_certificate(
-                DomainName=domainNames[x],
-                IdempotencyToken= str(x),
-                Options={
-                    'CertificateTransparencyLoggingPreference': 'ENABLED'
-                },
+        if SubArn is None:
+            print ("Error: Could not find subordinate certificate")
+        else:
+            #Get Subordinate CA CSR
+            csr = client.get_certificate_authority_csr(
                 CertificateAuthorityArn=SubArn
             )
-            tagResponse = acmClient.add_tags_to_certificate(
-                CertificateArn=response['CertificateArn'],
-                Tags=[
-                    {
-                        'Key': 'project',
-                        'Value': 'applicationV1'
+                
+            #Generate private end entity certificates
+            for x in range(0, numCerts):
+                response = acmClient.request_certificate(
+                    DomainName=domainNames[x],
+                    IdempotencyToken= str(x),
+                    Options={
+                        'CertificateTransparencyLoggingPreference': 'ENABLED'
                     },
-                ]
-            )
-            print(response['CertificateArn'])
-        print('Certificates created. Navigate to ACM Console.')
+                    CertificateAuthorityArn=SubArn
+                )
+                tagResponse = acmClient.add_tags_to_certificate(
+                    CertificateArn=response['CertificateArn'],
+                    Tags=[
+                        {
+                            'Key': 'project',
+                            'Value': 'applicationV1'
+                        },
+                    ]
+                )
+                print(response['CertificateArn'])
+            print('Certificates created. Navigate to ACM Console.')
     except:
         print("Unexpected error:", sys.exc_info()[0])
         raise
